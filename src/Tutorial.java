81ea8033bad2908f09b52df49467f56116e2ad06
Suraj-9891 <surajkrstm4002@gmail.com>
2024-11-20 16:16:35 +0530

    first commit

---
+++ src/Tutorial.java
@@ -0,0 +1,10 @@
+import  java.util.Scanner;
+public class Tutorial {
+    public static void main(String args[]){
+        int a;
+        System.out.println("Enter a Number : ");
+        Scanner obj = new Scanner(System.in);
+        a = obj.nextInt();
+        System.out.println("Get Data : " +a);
+    }
+}
